ec2d7bddd0995826010c111a9b621c8b310d4196
marleigh <volcana@gmail.com>
2023-02-18 12:02:03 -0800

    adding footer, removing links from nav to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Main from "./Main";
+
 
 function App() {
   return (
-    <div className="container mx-auto">
-      <Header/>
-      <Main />
-    </div>
+      <div className="container mx-auto">
+        <Header/>
+        <Main />
+        <Footer />
+      </div>
+    
   );
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,22 +7,10 @@ export default function Nav() {
     return (
         <ul className="hidden md:flex items-center justify-between">
             <li className='p-4'>
-                <a href="http://github.com/kittywizard/runaway-tales"
-                    target="_blank"
-                    rel="noreferrer"
-                    alt="Github"
-                >
-                    <FontAwesomeIcon icon={faGithub} className="text-3xl text-dark-green hover:text-gray-dark"/>
-                </a>
+                Login
             </li>
             <li>
-            <a href="https://autocode.com/app/kittywizard/basic-discord-example/"
-                    target="_blank"
-                    rel="noreferrer"
-                    alt="Discord Bot"
-                >
-                <FontAwesomeIcon icon={faDiscord} className="text-3xl text-dark-green hover:text-gray-dark"/>
-            </a>
+                Logout
             </li>
         </ul>
     )
