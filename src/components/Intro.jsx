857c7362d2146c792c9b05b4f0c55e9aaaee86ff
kittywizard <volcana@gmail.com>
2025-05-23 10:54:10 -0700

    intro display different message on login

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,6 +1,20 @@
+import { supabase } from "../supabaseClient";
+import { useEffect, useState } from "react";
+
 export default function Intro() {
+    const [getSession, setGetSession] = useState(false);
+
+    useEffect(() => {
+        supabase.auth.getSession().then((session) => {
+            if (session.data.session !== null) setGetSession(prevState => !prevState);
+        });
+    }, []);
+
     return (
         <div className="max-w-3xl mx-auto justify-center">
+        {getSession ?
+            <p>logged in</p>
+        :
             <p className="p-2 text-sm text-center">
                 Runaway Tales is a prompt based writing/art challenge, originally hosted on livejournal, now hosted at  
                 <a href="http://wriye.proboards.com" 
@@ -12,6 +26,8 @@ export default function Intro() {
                 There are over 3,500 prompts, separated into different 'flavors' based around themes (like emotions, props and actions). 
                 Pick one and get started!
             </p>
+        }
+            
             <p className="p-2 text-sm text-center">
                 You can narrow your results by selecting from one of the dropdowns below - either by specific theme (Action, Sensory, Romantic, etc.) 
                 or by a specific flavor (Vanilla, Chocolate, Strawberry, etc.) By leaving both blank, you'll get a random prompt from the entire list.
