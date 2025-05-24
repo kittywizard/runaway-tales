2865ce1b087e0929ced676f6dbfb2eff0d8c72c7
kittywizard <volcana@gmail.com>
2025-05-24 12:06:32 -0700

    saved prompts is working

--- src/components/SavedPrompts.jsx
+++ src/components/SavedPrompts.jsx
@@ -1,65 +1,67 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { supabase } from "../supabaseClient";
 import { useAuth } from "../auth/AuthContext";
-//import { Prompt } from "./Prompt";
+import { Context } from "../context";
+import  Prompt  from "./Prompt";
+import {nanoid } from "nanoid";
 
 export default function SavedPrompts() {
-       const {user} = useAuth();
-       const user_id = user.id;
+    const {user} = useAuth();
+    const user_id = user.id;
 
-        const [savedPrompts, setSavedPrompts] = useState([]);
+    const [savedPrompts, setSavedPrompts] = useState([]);
+    const {flavorsDB} = useContext(Context);
+
         
-            async function dataGrab(id) {
-                const {data: saved, error} = await supabase.from('saved')
-                .select('prompt_id')
-                .eq('user_id', id);
+    async function dataGrab(id) {
+        //grab specific user's prompt id list (aka. list of saved)
+        const {data: saved, error} = await supabase.from('saved')
+        .select('prompt_id')
+        .eq('user_id', id);
+
+        if(error) console.log(error)
+
+        //array of numbers for next query
+        let values = [];
+        let prompts = [];
+        for(let i = 0; i < saved.length; i++) {
+            values.push(parseInt(Object.values(saved[i])));
+        }
+
+        for (let i = 0; i < values.length; i++) {
+            prompts.push(flavorsDB[values[i]])
+        }
         
-                if(error) console.log(error)
+        setSavedPrompts(prompts);
+    }
 
-                let values = [];
+    
+    useEffect(()=> {
+        dataGrab(user_id);
+    },[]);
 
-                for(let i = 0; i < saved.length; i++) {
-                    let object = saved[i].prompt_id;
-                    const {prompt, promptError} = await supabase.from('prompts').select('*').eq('prompt_id', object);
-                    //values.push(parseInt(Object.values(saved[i])));
-                    setSavedPrompts(prevState => {
-                        return [
-                            ...prevState,
-                            prompt
-                        ]
-                    });
-                }
-                console.log(savedPrompts)        
 
-            }
-            
-            useEffect(()=> {
-                dataGrab(user_id);
-            },[]);
-
-            //console.log(savedPrompts)
-
-            //maybe check if there are any here first
-            // const promptMap = SavedPrompts.map(prompt => (
-            //     <Prompt 
-            //         data={prompt}
-            //         key={nanoid()}
-            //     />
-            // ));
+    //maybe check if there are any here first
+    const promptMap = savedPrompts.map(prompt => (
+        <Prompt 
+            data={prompt}
+            key={nanoid()}
+        />
+    ));
 
     return (
         <main className="container flex flex-col justify-between max-w-3xl mx-auto">
                 <h1 className="font-bold text-2xl text-dark-green text-center">Saved Prompts</h1>
-                <p className="p-2 text-sm text-left">
+                <div className="p-2 text-sm text-left">
                 {
                     savedPrompts == {} ? <>
                         Find some prompts!
                     </> :
                         <>
-                            no
+                            {promptMap}
                         </>
                 }
-                </p>
+                </div>
         </main>
 
     )
